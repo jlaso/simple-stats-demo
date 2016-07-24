3a56ea168fabe80d7f839122fec585218ecaa537
Joseluis Laso <jlaso@joseluislaso.es>
2016-07-24 21:09:12 +0200

    adding simple logger

--- app/Command/PostDeployCommand.php
+++ app/Command/PostDeployCommand.php
@@ -6,6 +6,7 @@ use JLaso\SimpleStats\Stats;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use JLaso\SimpleLogger\PlainFileLogger as Logger;
 
 class PostDeployCommand extends Command
 {
@@ -21,14 +22,24 @@ class PostDeployCommand extends Command
     {
         $start = microtime(true);
 
+        $folders = array(
+            '/public/cache' => 0777,
+            '/app/cache' => 0777,
+            'app/db' => 0777,
+        );
         $rootDir = dirname(dirname(dirname(__DIR__)));
-        @mkdir("{$rootDir}/public/cache", 0777, true);
-        @mkdir("{$rootDir}/app/cache", 0777, true);
-        @mkdir("{$rootDir}/app/db", 0777, true);
-
+        $logger = Logger::getInstance();
+        foreach($folders as $folder=>$perms) {
+            $logger->info("Creating folder {$folder}");
+            @mkdir($rootDir.$folder, $perms, true);
+        }
+        
         // force creation of DB
         $stats = new Stats();
-
-        $output->writeln('Process completed in '.intval((microtime(true)-$start)*1000).' msec');
+        $logger->info("Created sqlite database file on ".$stats->getDataBaseFile());
+        
+        $timeTaken = intval((microtime(true)-$start)*1000);
+        $logger->info("Process completed in {$timeTaken} msec");
+        $output->writeln("Process completed in {$timeTaken} msec");
     }
 }
